525a1b55e095ce852ab3c9b535d5c427c8f62e8a
georgeIsaev <44908722+georgeIsaev@users.noreply.github.com>
2018-11-18 02:42:43 +0300

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,222 +1,225 @@
+var bracketsData = [
+	{
+		text: 'Кронштейн',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/zuPskqnmIhk3b8V'
+	},
+
+	{
+		text: 'Кронштейн 3',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/lIASCLKpFkbfSRZ'
+	},
+
+	{
+		text: 'Кронштейн 4',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/A5jM9CbEbkqanyv'
+	},
+
+	{
+		text: 'Кронштейн 135°',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/rX9o4Esy9FjYfJ4'
+	},
+
+	{
+		text: 'Кронштейн Г-образный большой',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/7u3zVsm9UeRE0qi'
+	},
+
+	{
+		text: 'Кронштейн Г-образный средний',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/5Dfmoqqur3vcqms'
+	},
+
+	{
+		text: 'Кронштейн Г-образный малый',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/qH8RkWViGdVNkvH'
+	},
+
+	{
+		text: 'Кронштейн треугольный большой левый',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/45GLbESn9NhL4NJ'
+	},
+
+	{
+		text: 'Кронштейн треугольный большой правый',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/TwjzfBCFNUolZxR'
+	},
+
+	{
+		text: 'Кронштейн треугольный малый левый',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/rN4LjHCiNGLcfaB'
+	},
+
+	{
+		text: 'Кронштейн треугольный малый правый',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/FuaIqT8eHh2Hd0k'
+	},
+
+	{
+		text: 'Кронштейн треугольный средний левый',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/fbT0sjjwqP6RKsA'
+	},
+
+	{
+		text: 'Кронштейн треугольный средний правый',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/svpDjipATdFg9SV'
+	},
+
+	{
+		text: 'Кронштейн петля',
+		class: 'bracket',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/2ylmJ5dJHdBeMU6'
+	}
+];
+
+var railsData = [
+	{
+		text: 'Рейка',
+		class: 'rail',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/HAAgOBPzNumJozH'
+	},
+
+	{
+		text: 'Рейка большая',
+		class: 'rail',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/POILtMf3Kw1xeJn'
+	},
+
+	{
+		text: 'Рейка средняя',
+		class: 'rail',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/2BRbYxao3rR68hw'
+	},
+
+	{
+		text: 'Рейка малая',
+		class: 'rail',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/F3EaqMYJgvXUaPh'
+	}
+];
+
+var platsData = [
+	{
+		text: 'Пластина резная 400',
+		class: 'plat',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/F3EaqMYJgvXUaPh'
+	},
+
+	{
+		text: 'Пластина резная 460',
+		class: 'plat',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Пластина уголок большая',
+		class: 'plat',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Пластина уголок средняя',
+		class: 'plat',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Пластина уголок малая',
+		class: 'plat',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Т-образная пластина',
+		class: 'plat',
+		modelSrc: ''
+	},
+];
+
+var cornersData = [
+	{
+		text: 'Сборка большая',
+		class: 'corner',
+		modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/F3EaqMYJgvXUaPh'
+	},
+
+	{
+		text: 'Сборка средняя',
+		class: 'corner',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Сборка малая',
+		class: 'corner',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Уголок большой',
+		class: 'corner',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Уголок средний',
+		class: 'corner',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Уголок малый',
+		class: 'corner',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Уголок оконный большой',
+		class: 'corner',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Уголок оконный малый',
+		class: 'corner',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Уголок оконный средний',
+		class: 'corner',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Уголок с овальными отверстиями',
+		class: 'corner',
+		modelSrc: ''
+	},
+
+	{
+		text: 'Уголок трехмерный',
+		class: 'corner',
+		modelSrc: ''
+	},
+];
+
+
+
 $(function() {
-	var bracketsData = [
-		{
-			text: 'Кронштейн',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/zuPskqnmIhk3b8V'
-		},
-
-		{
-			text: 'Кронштейн 3',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/lIASCLKpFkbfSRZ'
-		},
-
-		{
-			text: 'Кронштейн 4',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/A5jM9CbEbkqanyv'
-		},
-
-		{
-			text: 'Кронштейн 135°',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/rX9o4Esy9FjYfJ4'
-		},
-
-		{
-			text: 'Кронштейн Г-образный большой',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/7u3zVsm9UeRE0qi'
-		},
-
-		{
-			text: 'Кронштейн Г-образный средний',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/5Dfmoqqur3vcqms'
-		},
-
-		{
-			text: 'Кронштейн Г-образный малый',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/qH8RkWViGdVNkvH'
-		},
-
-		{
-			text: 'Кронштейн треугольный большой левый',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/45GLbESn9NhL4NJ'
-		},
-
-		{
-			text: 'Кронштейн треугольный большой правый',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/TwjzfBCFNUolZxR'
-		},
-
-		{
-			text: 'Кронштейн треугольный малый левый',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/rN4LjHCiNGLcfaB'
-		},
-
-		{
-			text: 'Кронштейн треугольный малый правый',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/FuaIqT8eHh2Hd0k'
-		},
-
-		{
-			text: 'Кронштейн треугольный средний левый',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/fbT0sjjwqP6RKsA'
-		},
-
-		{
-			text: 'Кронштейн треугольный средний правый',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/svpDjipATdFg9SV'
-		},
-
-		{
-			text: 'Кронштейн петля',
-			class: 'bracket',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/2ylmJ5dJHdBeMU6'
-		}
-	];
-
-	var railsData = [
-		{
-			text: 'Рейка',
-			class: 'rail',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/HAAgOBPzNumJozH'
-		},
-
-		{
-			text: 'Рейка большая',
-			class: 'rail',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/POILtMf3Kw1xeJn'
-		},
-
-		{
-			text: 'Рейка средняя',
-			class: 'rail',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/2BRbYxao3rR68hw'
-		},
-
-		{
-			text: 'Рейка малая',
-			class: 'rail',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/F3EaqMYJgvXUaPh'
-		}
-	];
-
-	var platsData = [
-		{
-			text: 'Пластина резная 400',
-			class: 'plat',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/F3EaqMYJgvXUaPh'
-		},
-
-		{
-			text: 'Пластина резная 460',
-			class: 'plat',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Пластина уголок большая',
-			class: 'plat',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Пластина уголок средняя',
-			class: 'plat',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Пластина уголок малая',
-			class: 'plat',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Т-образная пластина',
-			class: 'plat',
-			modelSrc: ''
-		},
-	];
-
-	var cornersData = [
-		{
-			text: 'Сборка большая',
-			class: 'corner',
-			modelSrc: 'https://www.3dvieweronline.com/members/Id64329af3e5dc362feda36f2bb623a515/F3EaqMYJgvXUaPh'
-		},
-
-		{
-			text: 'Сборка средняя',
-			class: 'corner',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Сборка малая',
-			class: 'corner',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Уголок большой',
-			class: 'corner',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Уголок средний',
-			class: 'corner',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Уголок малый',
-			class: 'corner',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Уголок оконный большой',
-			class: 'corner',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Уголок оконный малый',
-			class: 'corner',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Уголок оконный средний',
-			class: 'corner',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Уголок с овальными отверстиями',
-			class: 'corner',
-			modelSrc: ''
-		},
-
-		{
-			text: 'Уголок трехмерный',
-			class: 'corner',
-			modelSrc: ''
-		},
-	];
-
+	
 	// Функция создания елементов
 	var makeElement = function (tagName, className, text) {
 	  var element = document.createElement(tagName);
@@ -230,12 +233,12 @@ $(function() {
 	// Функция создания елементов списка
 	var createPar = function(liName){
 		var paragraph = makeElement('li', liName.class);
+		paragraph.classList.add('list-group-item');
 		var link = makeElement('a', 'br_link',liName.text);
 		link.href = '#';
 		paragraph.appendChild(link);
 		return paragraph;
 	};
-
 
 
 	// Выбор елемента Кронштейны
@@ -277,9 +280,11 @@ $(function() {
 		listElement.appendChild(app);
 
 	}
-
-
+});
 	
+	
+$(function() {
+
 	var modelFrame = document.getElementById('3dviewerplayer');
 
 	//Выбор всех элементов списка Кронштейны, Рейка, Пластины, Уголок
@@ -311,5 +316,6 @@ $(function() {
 	for (var i = 0; i < cornersList.length; i++) {
   	ClickHandler(cornersList[i], cornersData[i]);
 	}
-
 });
+	
+
